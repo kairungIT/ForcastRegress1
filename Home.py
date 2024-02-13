3c3e61f1c5c706df50366be3bb2e39251e9b62f5
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-02-13 15:34:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,5 +15,9 @@ def load_lottieurl(url: str):
 lottie_url_hello = "https://lottie.host/7beb1022-e2a7-4d35-be1e-a4f8372bd478/fPwMwU2mVy.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
+st.title("การพยากรณ์ข้อมูลด้วยเทคนิค Linear Regression")
+st.header("by Kairung Hengpraprohm")
+
+#การเรียกใช้งาาน lottie file
 st_lottie(lottie_hello, key="hello")
 st.balloons()
