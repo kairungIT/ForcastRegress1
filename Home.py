d0c704d31cae756fe3a0474f6caa53dbe4bbb0c9
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-02-13 15:24:15 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/8a50ffe1-21f4-466b-828a-4d8380f19ae7/I8vj68E5IJ.json"
 lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 lottie_download = load_lottieurl(lottie_url_download)
